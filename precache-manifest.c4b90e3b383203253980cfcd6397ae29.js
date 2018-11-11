aa97e7e121c310f47d1b4a7771fc2b95ccbd096f
Duy GG <duy_gg9124@co-well.com.vn>
2018-11-12 01:16:48 +0700

    Updates

---
+++ precache-manifest.c4b90e3b383203253980cfcd6397ae29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a4e5c756d9e8babc7374",
+    "url": "/reactjs/static/js/runtime~main.a4e5c756.js"
+  },
+  {
+    "revision": "326a7b50889cb5ec6f05",
+    "url": "/reactjs/static/js/main.326a7b50.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/reactjs/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "326a7b50889cb5ec6f05",
+    "url": "/reactjs/static/css/main.0f4e7e07.chunk.css"
+  },
+  {
+    "revision": "d3445a6efe7f4212dc5996ab622c0db0",
+    "url": "/reactjs/index.html"
+  }
+];
